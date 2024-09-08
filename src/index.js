b9615e01f6d3a5a94fba73401b0619125295fdd9
vazcon <zaidniaazi@gmail.com>
2024-09-08 09:51:49 +0530

    add eventHandler

--- src/index.js
+++ src/index.js
@@ -2,6 +2,25 @@ import manPicture from "./man.jpg";
 import { contentLoader } from "./content.js";
 
 const { headLine, introH2, paragraph } = contentLoader();
+const buttons = document.querySelectorAll("button");
+
+function eventHandler(event) {
+  const buttonId = event.target.id;
+  switch (buttonId) {
+    case "home":
+      console.log("Home");
+      break;
+    case "menu":
+      console.log("menu");
+      break;
+    case "about":
+      console.log("about");
+
+      break;
+  }
+}
+
+buttons.forEach((button) => button.addEventListener("click", eventHandler));
 
 const intro = document.querySelector("#content");
 const headText = document.createElement("h1");
